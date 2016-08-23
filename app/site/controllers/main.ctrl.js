1e957982d751d8d341e67fd6a1634183b2bbf405
Amanda Field <afield@brainstation.io>
2016-08-23 00:06:47 -0400

    Full logo and meeting time position

--- app/site/controllers/main.ctrl.js
+++ app/site/controllers/main.ctrl.js
@@ -1,11 +1,11 @@
-(function() {
+(function () {
     'use strict';
 
     app.controller('MainCtrl', MainCtrl);
 
     function MainCtrl($state) {
         var mainVm = this;
-        mainVm.meetingDate = "Tuesday September 6th, 2016";
+        mainVm.meetingDate = "September 6th";
         mainVm.meetingTime = "6:00PM";
         mainVm.meetingRoom = "E212";
 
@@ -17,24 +17,24 @@
 
         function go(location) {
             switch (location) {
-                case 'home':
-                    $state.go('home');
-                    break;
-                case 'about':
-                    $state.go('about');
-                    break;
-                case 'contact':
-                    $state.go('contact');
-                    break;
-                case 'messageboard':
-                    $state.go('messageboard');
-                    break;
-                case 'admin.panel':
-                    $state.go('admin.panel');
-                    break;
-                case '404':
-                    $state.go('404');
-                    break;
+            case 'home':
+                $state.go('home');
+                break;
+            case 'about':
+                $state.go('about');
+                break;
+            case 'contact':
+                $state.go('contact');
+                break;
+            case 'messageboard':
+                $state.go('messageboard');
+                break;
+            case 'admin.panel':
+                $state.go('admin.panel');
+                break;
+            case '404':
+                $state.go('404');
+                break;
             }
         }
 

e396208941adac7ac6cfda4e24d15ffd3742e5a7
Amanda Field <afield@brainstation.io>
2016-08-23 02:33:16 -0400

    Revert "using nodemail instead of php - banner message added aswell"
    This reverts commit a29da2c1f27df66572c6c70e2eddc5104aca1146.

--- api/routes/user_routes.js
+++ api/routes/user_routes.js
@@ -7,6 +7,14 @@ var nodemailer = require('nodemailer');
 var bcrypt = require('bcryptjs');
 var transporter = nodemailer.createTransport('smtps://afield788%40gmail.com:<password>@smtp.gmail.com');
 
+//// setup e-mail data with unicode symbols
+//var mailOptions = {
+//    from: '"Platform" <amanda@platform.com>', // sender address
+//    to: newUser.email, // list of receivers
+//    subject: 'Hello ' + newUser.name, // Subject line
+//    text: 'You have logged in', // plaintext body
+//};
+
 router.get('/users/', function (req, res) {
 
     User.find({}, function (err, users) {
@@ -26,8 +34,8 @@ router.post('/register', function (req, res) {
 
     //check if user is already registered
     User.findOne({
-            'email': __user.email
-        })
+        'email': __user.email
+    })
         .then(function (user) {
             if (!user) {
                 //user does not exist
@@ -37,18 +45,19 @@ router.post('/register', function (req, res) {
                     email: __user.email,
                     password: __user.password
                 });
+                console.log(newUser);
 
                 //encrypt password
                 bcrypt.genSalt(12, function (err, salt) {
                     bcrypt.hash(__user.password, salt, function (err, hash) {
-
+                        
                         // Store hash in your password DB.
                         console.log(hash);
                         newUser.password = hash;
-
+                        
                         newUser.save(newUser)
                             .then(function (user) {
-
+                                
                                 //remove password from response
                                 delete user.password;
                                 res.json({
@@ -77,53 +86,53 @@ router.post('/register', function (req, res) {
 });
 
 router.post('/authenticate', function (req, res) {
-        console.log('Authentication Endpoint');
-        var __user = req.body;
-        User.findOne({
-                'email': __user.email
-            })
-            .then(function (user) {
-                if (user) {
-                    console.log(user);
-                    //check incoming password against encrypted version
-                    bcrypt.compare(__user.password, user.password, function (err, valid) {
-                        if (valid) {
-                            //remove password from response
-                            delete user.password;
-                            //set web token
-                            var user_obj = {
-                                firstName: user.firstName,
-                                lastName: user.lastName,
-                                email: user.email
-                            };
-                            var token = jwt.sign(user_obj, 'randomsalt');
-                            res.set('authentication', token);
-                            res.json({
-                                user: user,
-                                msg: 'Authenticated'
-                            });
-                        } else {
-                            res.json({
-                                user: null,
-                                msg: 'Email/Password is incorrect'
-                            })
-                        }
-                    });
-                } else {
-                    res.json({
-                        user: null,
-                        msg: 'Email/Password is incorrect'
-                    })
-                }
-            })
-
+    console.log('Authentication Endpoint');
+    var __user = req.body;
+    User.findOne({
+        'email': __user.email
     })
-    // send mail with defined transport object
-    //transporter.sendMail(mailOptions, function(error, info){
-    //    if(error){
-    //        return console.log(error);
-    //    }
-    //    console.log('Message sent: ' + info.response);
-    //});
+        .then(function (user) {
+            if (user) {
+                console.log(user);
+                //check incoming password against encrypted version
+                bcrypt.compare(__user.password, user.password, function (err, valid) {
+                    if (valid) {
+                        //remove password from response
+                        delete user.password;
+                        //set web token
+                        var user_obj = {
+                            firstName: user.firstName,
+                            lastName: user.lastName,
+                            email: user.email
+                        };
+                        var token = jwt.sign(user_obj, 'randomsalt');
+                        res.set('authentication', token);
+                        res.json({
+                            user: user,
+                            msg: 'Authenticated'
+                        });
+                    } else {
+                        res.json({
+                            user: null,
+                            msg: 'Email/Password is incorrect'
+                        })
+                    }
+                });
+            } else {
+                res.json({
+                    user: null,
+                    msg: 'Email/Password is incorrect'
+                })
+            }
+        })
+
+})
+// send mail with defined transport object
+//transporter.sendMail(mailOptions, function(error, info){
+//    if(error){
+//        return console.log(error);
+//    }
+//    console.log('Message sent: ' + info.response);
+//});
 
 module.exports = router;
--- app/site/controllers/main.ctrl.js
+++ app/site/controllers/main.ctrl.js
@@ -3,20 +3,15 @@
 
     app.controller('MainCtrl', MainCtrl);
 
-    function MainCtrl($state, api) {
+    function MainCtrl($state) {
         var mainVm = this;
         mainVm.meetingDate = "September 6th";
         mainVm.meetingTime = "6:00PM";
         mainVm.meetingRoom = "E212";
 
-        //for alert box when email is sent
-        mainVm.showAlert = false;
-
         mainVm.state = $state;
         mainVm.load = load;
         mainVm.go = go;
-        mainVm.sendEmail = sendEmail;
-        mainVm.clearForm = clearForm;
 
         mainVm.load();
 
@@ -49,41 +44,6 @@
         function load() {
             $('.main-slider').addClass('animate-in');
             $('.preloader').remove();
-        }
-
-        function sendEmail(name, email, message) {
-            var payload = {
-                name: name,
-                email: email,
-                message: message
-            }
-            api.request('/sendEmail', payload, 'POST')
-                .then(function (res) {
-                    if (res.status == 200) {
-                        console.log(res.data.msg);
-                        mainVm.alertClass = "alert-success";
-
-                        mainVm.emailMessage = res.data.msg + " from " +
-                            res.data.user.email;
-
-                    } else {
-                        console.log(res.data.msg);
-                        mainVm.alertClass = "alert-danger";
-
-                        mainVm.emailMessage = res.data.msg + " from " + email + " - Please review your information and try again.";
-
-                    }
-                    mainVm.showAlert = true;
-
-
-                    mainVm.clearForm();
-                })
-        }
-
-        function clearForm() {
-            mainVm.contactName = "";
-            mainVm.contactEmail = "";
-            mainVm.contactMessage = "";
         }
     }
 })();

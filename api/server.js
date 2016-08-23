a29da2c1f27df66572c6c70e2eddc5104aca1146
Amanda Field <afield@brainstation.io>
2016-08-23 02:31:08 -0400

    using nodemail instead of php - banner message added aswell

--- api/server.js
+++ api/server.js
@@ -19,29 +19,32 @@ app.use(express.static(__dirname + './../app/'));
 mongoose.connect('mongodb://localhost/data/db/');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', function () {
     console.log("Connected to db at /data/db/");
 });
 
 //Routes
 var user_routes = require('./routes/user_routes.js');
 app.use('/', user_routes);
+var public_routes = require('./routes/public_routes.js');
+app.use('/', public_routes);
+
 //
 //var upload_routes = require('./routes/upload_routes.js');
 //app.use('/', upload_routes);
 //var message_routes = require('./routes/message_routes.js');
 //app.use('/', message_routes);
 
-app.on('close', function() {
+app.on('close', function () {
     console.error('dropping db');
-    db.db.dropDatabase(function() {
+    db.db.dropDatabase(function () {
         console.error('closing db connection');
         mongoose.connection.close();
     })
 })
 
 //Connection
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('Listening on http://localhost:' + port);
     console.log('Stop Server with CTRL + C');
 });

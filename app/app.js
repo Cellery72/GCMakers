631a2e1d4c2391fb9d162b07e36ebb997cea06ed
Amanda Field <afield@brainstation.io>
2016-08-23 00:52:31 -0400

    angularizing gallery

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 var app = angular.module('makers', ['ui.router', 'ui.bootstrap', 'angular-jwt']);
 
-app.config(function($stateProvider, $httpProvider, $urlRouterProvider) {
+app.config(function ($stateProvider, $httpProvider, $urlRouterProvider) {
     // defer no route to home route
     $urlRouterProvider.when('', '/');
     // defer all other garbage to 404
@@ -81,6 +81,23 @@ app.config(function($stateProvider, $httpProvider, $urlRouterProvider) {
                 }
             }
         })
+        .state('gallery', {
+            url: '/gallery',
+            views: {
+                header: {
+                    templateUrl: 'site/partials/common/header.html',
+                    controller: 'MainCtrl as ctrl'
+                },
+                layout: {
+                    templateUrl: 'site/partials/gallery.html',
+                    controller: 'MainCtrl as ctrl'
+                },
+                footer: {
+                    templateUrl: 'site/partials/common/footer.html',
+                    controller: 'MainCtrl as ctrl'
+                }
+            }
+        })
         .state('admin', {
             url: '/admin',
             controller: 'AdminCtrl as ctrl',
@@ -122,30 +139,30 @@ app.config(function($stateProvider, $httpProvider, $urlRouterProvider) {
             parent: 'admin'
         })
         .state('404', {
-          url: '/404',
-          views: {
-            header:{
-                templateUrl: 'site/partials/common/header.html',
-                controller: 'MainCtrl as ctrl'
-            },
-            layout: {
-              templateUrl: 'site/partials/404.html',
-              controller: 'MainCtrl as ctrl'
-            },
-            footer: {
-              templateUrl: 'site/partials/common/footer.html',
-              controller: 'MainCtrl as ctrl'
+            url: '/404',
+            views: {
+                header: {
+                    templateUrl: 'site/partials/common/header.html',
+                    controller: 'MainCtrl as ctrl'
+                },
+                layout: {
+                    templateUrl: 'site/partials/404.html',
+                    controller: 'MainCtrl as ctrl'
+                },
+                footer: {
+                    templateUrl: 'site/partials/common/footer.html',
+                    controller: 'MainCtrl as ctrl'
+                }
             }
-          }
         })
 
-    $httpProvider.interceptors.push(function(jwtHelper) {
+    $httpProvider.interceptors.push(function (jwtHelper) {
         return {
-            request: function(config) {
+            request: function (config) {
                 config.headers.authentication = localStorage.authToken;
                 return config;
             },
-            response: function(response) {
+            response: function (response) {
                 var auth_token = response.headers('authentication');
                 if (auth_token) {
                     var decrypt_token = jwtHelper.decodeToken(auth_token);

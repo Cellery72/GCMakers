6c8e170d8cc767a60fc420d5ba980454e8c5e975
Justin <justin_ellery@hotmail.ca>
2016-09-20 18:36:20 -0400

    Made some changes to the email-gatherer form

--- email-gatherer/site/main.ctrl.js
+++ email-gatherer/site/main.ctrl.js
@@ -5,48 +5,12 @@
 
     function MainCtrl($state) {
         var mainVm = this;
-
-        //for alert box when email is sent
-        mainVm.showAlert = false;
-
         mainVm.state = $state;
-        mainVm.sendEmail = sendEmail;
-        mainVm.clearForm = clearForm;
+        mainVm.submitForm = submitForm();
 
-        function sendEmail(name, email, message) {
-            // var payload = {
-            //     name: name,
-            //     email: email,
-            //     message: message
-            // }
-            // api.request('/sendEmail', payload, 'POST')
-            //     .then(function(res) {
-            //         if (res.data.user == null) {
-            //             console.log(res.data.msg);
-            //             mainVm.alertClass = "alert-danger";
-
-            //             mainVm.emailMessage = res.data.msg + " - Please review your information and try again.";
-
-
-            //         } else {
-            //             console.log(res.data.msg);
-            //             mainVm.alertClass = "alert-success";
-
-            //             mainVm.emailMessage = res.data.msg + " from " +
-            //                 res.data.user.email;
-
-            //         }
-            //         mainVm.showAlert = true;
-
-
-            //         mainVm.clearForm();
-            //     })
-        }
-
-        function clearForm() {
-            mainVm.contactName = "";
-            mainVm.contactEmail = "";
-            mainVm.contactMessage = "";
+        function submitForm(maker) {
+          console.log('made it');
+          console.log(maker);
         }
     }
 })();
